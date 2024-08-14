3747b40d180c5198a38f6382d487d75ba37ed005
Muhammed Konulga <konulga0@gmail.com>
2024-08-14 11:22:58 +0300

    User model updated.

--- zuby_commerce/settings.py
+++ zuby_commerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'zuby_commerce.urls'
+
+AUTH_USER_MODEL = 'core.CustomUser'
 
 TEMPLATES = [
     {
@@ -113,9 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr-tr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
+
 
 USE_I18N = True
 
